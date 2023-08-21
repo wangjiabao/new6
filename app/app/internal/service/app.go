d60ebeb91f73b3e60119f8acd89c9d6a3d99e10d
wangjiabao <840028401@qq.com>
2023-08-22 02:15:15 +0800

    ok

--- app/app/internal/service/app.go
+++ app/app/internal/service/app.go
@@ -79,7 +79,7 @@ func (a *AppService) Deposit(ctx context.Context, req *v1.DepositRequest) (*v1.D
 	for i := 1; i <= 5; i++ {
 		depositUsdtResult, err = requestEthDepositResult(200, int64(i),
 			"0x55d398326f99059fF775485246999027B3197955",
-			"0x983a6385BBAC74476D538AD6961920925B617335",
+			"0x983a6385bbac74476d538ad6961920925b617335",
 		)
 		if nil != err {
 			break
